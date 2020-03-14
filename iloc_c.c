66846cdb3fca39aa6b8ca7441aa11bd9d3622a14
Serge Shary <5132720@gmail.com>
2020-03-14 22:08:38 +0600

    test run -gitignore(-csv)

--- iloc_c.c
+++ iloc_c.c
@@ -4,10 +4,10 @@ int main(int argc, char* argv[])
 {
     //2 arg = path to dataFile
     char const* const dataFile = argv[1];
-    FILE* hFile = fopen(dataFile, 'r') ;
+    FILE* hFile = fopen(dataFile, "r") ;
     
     char curLine[256];
-    while (fgest(curLine, sizeof(curLine), hFile)){
+    while (fgets(curLine, sizeof(curLine), hFile)){
         printf("%s", curLine);
     }
     fclose(hFile);
