02a4053a51d86b4720843661e0ef3b9359f95e73
Serge Shary <5132720@gmail.com>
2020-03-14 22:54:47 +0600

    add speed test

--- iloc_c.c
+++ iloc_c.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 const char* iloc(char* line, int index)
 {
@@ -8,6 +9,8 @@ const char* iloc(char* line, int index)
 }
 int main(int argc, char* argv[])
 {
+    //test
+    clock_t start =  clock();
     //2 arg = path to dataFile
     char const* const dataFile = argv[1];
     //3 arg = column index
@@ -21,8 +24,13 @@ int main(int argc, char* argv[])
     while (fgets(curLine, sizeof(curLine), hFile)){
         //printf("%s", curLine);
         char* tmp = strdup(curLine);
+
         free(tmp);    
     }
     fclose(hFile);
+
+    clock_t end = clock();
+    printf("elapsed time: [%.2f]ms\n", (double)((end-start)*1000/CLOCKS_PER_SEC));
+
     return 0;
 }
