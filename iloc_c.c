b8ed0927110130f5ea4d7265375a7ca35e08a721
Serge Shary <5132720@gmail.com>
2020-03-14 21:59:03 +0600

    add read file

---
+++ iloc_c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    //2 arg = path to dataFile
+    char const* const dataFile = argv[1];
+    FILE* hFile = fopen(dataFile, 'r') ;
+    
+    char curLine[256];
+    while (fgest(curLine, sizeof(curLine), hFile)){
+        printf("%s", curLine);
+    }
+    fclose(hFile);
+    return 0;
+}
